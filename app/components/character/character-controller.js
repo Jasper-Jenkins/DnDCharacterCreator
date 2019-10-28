3ddc26d3ff83c30d774f789b46e109c043b85ee6
Jasper Jenkins <ownyourfate@yahoo.com>
2019-10-27 22:50:13 -0600

    formatting

--- app/components/character/character-controller.js
+++ app/components/character/character-controller.js
@@ -2,23 +2,36 @@ import CharacterService from "./character-service.js";
 
 const _characterService = new CharacterService()
 
-function drawCharacter() {
-    console.log("The Character info:", _characterService)
+function drawRaces() {
+    console.log("Service when drwaRaces is called:", _characterService)
     var races = _characterService.Races;
     
     var template = '';
     for (var i = 0; i <races.count ; i++) {
 
-        template += `<div class="col race text-center">${races.races[i].name}</div>`
+        template += `<div class="col race text-center"><p>${races.races[i].name}</p></div>`
 
     }   
+    document.getElementById('chooseRace').innerHTML = template;
+}
+
+function drawClasses() {
+    console.log("Service when drawClasses is called:", _characterService)
+    var races = _characterService.Races;
+
+    var template = '';
+    for (var i = 0; i < races.count; i++) {
+
+        template += `<div class="col race text-center">${races.races[i].name}</div>`
+
+    }
     document.getElementById('chooseRace').innerHTML = template;
 }
 
 export default class CharacterController {
 
     constructor() {
-        _characterService.addSubscriber('races', drawCharacter)
+        _characterService.addSubscriber('races', drawRaces)
         _characterService.getAllRaces()
     }
     /*
